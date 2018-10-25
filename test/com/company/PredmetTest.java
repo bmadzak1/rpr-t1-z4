0f327588c71949ff343b2a76dee4d165857b3c59
bmadzak1 <bmadzak1@etf.unsa.ba>
2018-10-25 20:37:40 +0200

    Dodan test za trenutni broj studenata

--- test/com/company/PredmetTest.java
+++ test/com/company/PredmetTest.java
@@ -15,6 +15,21 @@ class PredmetTest {
     }
 
     @Test
+    void brojStudenataTest(){
+        Student student = new Student("Berin", "Madzak", 17897);
+        Predmet predmet = new Predmet("RPR", 12345, 120);
+        predmet.upisi(student);
+        Student student2 = new Student("Berin2", "Madzak", 17888);
+        Student student3 = new Student("Berin3", "Madzak", 17297);
+        predmet.upisi(student2);
+        predmet.upisi(student3);
+        assertEquals(3, predmet.dajTrenutniBrojStudenata());
+        predmet.ispisi(student3);
+        predmet.ispisi(student);
+        assertEquals(1, predmet.dajTrenutniBrojStudenata());
+    }
+
+    @Test
     void upisStudentaTest(){
         Student student = new Student("Berin", "Madzak", 17897);
         Predmet predmet = new Predmet("RPR", 12345, 120);

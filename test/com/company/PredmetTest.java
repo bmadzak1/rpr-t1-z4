082269851aa1a28789a8478da67cb4d8127c4d80
bmadzak1 <bmadzak1@etf.unsa.ba>
2018-10-25 20:26:11 +0200

    Dodan test za konstruktor klase Predmet

--- test/com/company/PredmetTest.java
+++ test/com/company/PredmetTest.java
@@ -1,7 +1,17 @@
 package com.company;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PredmetTest {
+
+    @Test
+    void testKonstruktora(){
+        Predmet noviPredmet = new Predmet("RPR", 12345, 120);
+        assertEquals("RPR", noviPredmet.nazivPredmeta);
+        assertEquals(12345, noviPredmet.sifraPredmeta);
+        assertEquals(120, noviPredmet.maksimalanBrojStudenata);
+    }
 
 }

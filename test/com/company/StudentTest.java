b32618a6daeee35dfab4bc231b2e15323b4410df
bmadzak1 <bmadzak1@etf.unsa.ba>
2018-10-25 20:45:51 +0200

    Dodan test za promjenu podataka klase Student

--- test/com/company/StudentTest.java
+++ test/com/company/StudentTest.java
@@ -20,4 +20,15 @@ class StudentTest {
         assertEquals("Madzak Berin (17897)", student.toString());
     }
 
+    @Test
+    void promjenaPodatakaTest(){
+        Student student = new Student("Berin", "Madzak", 17897);
+        student.promijeniIme("Nireb");
+        assertEquals("Nireb", student.ime);
+        student.promijeniPrezime("Kadzam");
+        assertEquals("Kadzam", student.prezime);
+        student.promijeniBrojIndeksa(79871);
+        assertEquals(79871, student.brojIndeksa);
+    }
+
 }
